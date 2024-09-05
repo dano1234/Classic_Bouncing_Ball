b7111446a4af8a2fb431e81d9c30dc32a095eaba
Daniel O'Sullivan <dan.osullivan@nyu.edu>
2024-09-05 12:20:17 -0400

    made it purple

--- sketch.js
+++ sketch.js
@@ -7,23 +7,23 @@ function setup() {
   createCanvas(400, 400);
   xLoc = 0;
   yLoc = 100;
-  directionX = 5	;
-  directionY = 5	;
+  directionX = 5;
+  directionY = 5;
 }
 
 function draw() {
   background(255);
-  
+
   xLoc = xLoc + directionX;
-  if (xLoc < 0 || xLoc > width){
-		directionX = -directionX
+  if (xLoc < 0 || xLoc > width) {
+    directionX = -directionX
   }
   yLoc = yLoc + directionY;
-  if (yLoc < 0 || yLoc > height){
-		directionY = -directionY
+  if (yLoc < 0 || yLoc > height) {
+    directionY = -directionY
   }
-  fill(0,255,0);
-  ellipse(xLoc,yLoc, 20,20);
-  
-  
+  fill(255, 0, 255);
+  ellipse(xLoc, yLoc, 20, 20);
+
+
 }
