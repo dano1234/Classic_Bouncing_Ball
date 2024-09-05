b3d957ccd02ac5aebcc9286267100c1bbf5c58a5
Daniel O'Sullivan <dan.osullivan@nyu.edu>
2024-09-05 12:17:31 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var xLoc;
+var yLoc;
+var directionX;
+var directionY;
+
+function setup() {
+  createCanvas(400, 400);
+  xLoc = 0;
+  yLoc = 100;
+  directionX = 5	;
+  directionY = 5	;
+}
+
+function draw() {
+  background(255);
+  
+  xLoc = xLoc + directionX;
+  if (xLoc < 0 || xLoc > width){
+		directionX = -directionX
+  }
+  yLoc = yLoc + directionY;
+  if (yLoc < 0 || yLoc > height){
+		directionY = -directionY
+  }
+  fill(0,255,0);
+  ellipse(xLoc,yLoc, 20,20);
+  
+  
+}
